2b03ee1c00f9760fb7157e00387560805cc3b743
monmon-2007 <minamonir11@hotmail.com>
2018-11-19 19:58:16 -0500

    Project started

---
+++ app/config/database.js
@@ -0,0 +1,7 @@
+const crypto = require('crypto').randomBytes(256).toString('hex');
+//uri: 'mongodb://localhost:27017/invest'
+module.exports={
+  uri: 'mongodb://admin:admin123@ds145355.mlab.com:45355/invest',
+  secret: crypto,
+  db:'invest'
+}
---
+++ app/routes/stocks.js
@@ -0,0 +1,20 @@
+module.exports = (router) =>{
+
+
+  router.get('/hey',function(req,res){
+    res.send("hello World")
+  })
+
+  router.post('/hello', function(req,res){
+    Robinhood.quote_data(req.body.ticker,function(error,response,body){
+      if(error)
+      {
+        console.log(error)
+        process.exit(1)
+      }
+      res.send(body)
+    })
+  })
+
+return router;
+}
