f67c30dab4f2aa18808cee267af01cc5f6c45bf8
Mohammad Salim <mfsalim27@gmail.com>
2018-11-19 22:49:09 -0500

    Fix trading view on search

--- client/src/app/components/dashboard/dashboard.component.ts
+++ client/src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 declare const TradingView: any;
 
@@ -7,7 +7,7 @@ declare const TradingView: any;
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements AfterViewInit, OnInit {
+export class DashboardComponent implements OnInit {
 
   form: FormGroup;
 
@@ -23,36 +23,34 @@ export class DashboardComponent implements AfterViewInit, OnInit {
 
   ngOnInit() { }
 
-  ngAfterViewInit() {
-    const tradingView = new TradingView({
-      'container_id': 'tradingview-widget-container',
-      'width': 425,
-      'height': 410,
-      'symbol': 'NASDAQ:AAPL',
-      'locale': 'en',
-      'interval': '1D'
-    });
+  updateTradingView(ticker: String) {
+    // const tradingView = new TradingView({
+    //   'container_id': 'tradingview-widget-container',
+    //   'width': 425,
+    //   'height': 410,
+    //   'symbol': 'NASDAQ:AAPL',
+    //   'locale': 'en',
+    //   'interval': '1D'
+    // });
 
-    /*  new TradingView.widget({
-        'container_id': 'myWidgetContainer',
-        'width': 550,
-        'height': 410,
-        'symbol': ticker,
-        'interval': 'D',
-        'timezone': 'Etc/UTC',
-        'theme': 'Dark',
-        'style': '1',
-        'locale': 'en',
-        'toolbar_bg': '#f1f3f6',
-        'enable_publishing': false,
-        'allow_symbol_change': true,
-        'hideideas': true
-      });
-    */
+    const tradingView = new TradingView.widget({
+      'container_id': 'myWidgetContainer',
+      'width': 550,
+      'height': 410,
+      'symbol': ticker,
+      'interval': 'D',
+      'timezone': 'Etc/UTC',
+      'theme': 'Dark',
+      'style': '1',
+      'locale': 'en',
+      'toolbar_bg': '#f1f3f6',
+      'enable_publishing': false,
+      'allow_symbol_change': true,
+      'hideideas': true
+    });
   }
 
   onSearchSubmit() {
-    console.log(this.form.get('search').value);
-    // this.ngAfterViewInit(this.form.get('search').value);
+    this.updateTradingView(this.form.get('search').value);
   }
 }
